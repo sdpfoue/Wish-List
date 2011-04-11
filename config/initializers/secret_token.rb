ac7d3dd8ad66b30d5fa07b70ece2e7c00b942755
Stephen <stephen.j.tong@gmail.com>
2011-04-12 07:42:50 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoSrc::Application.config.secret_token = '69faf7fe9c0f933d57bc1920a3f3ed33eaf937cbb6907f5ab6fa8954f0218ac05d9b7aad8e59b0c4c0c0417b24fbb4618b4b25785a24d16fa9473ce11fa9a3db'
